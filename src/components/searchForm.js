e1eabe73bd08588abe1ff8a38af590a6e318b2c7
Abhishek Kumar <abhishekrana201005@gmail.com>
2022-08-30 22:38:12 +0530

    done

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -27,35 +27,35 @@ const SearchForm = ({ onCompanyFilter, onRoleFilter, onLocationFilter }) => {
     }
   };
   return (
-    <div className="  mx-12 my-16 flex flex-row gap-x-3">
-      <div class="mb-3 pt-0">
-        <label>Company</label>
+    <div className="flex flex-col justify-center gap-x-5 bg-teal-200 shadow-md mx-12 my-6 p-6 rounded md:flex-col lg:flex-row">
+      <div className="mb-3 pt-0">
+        <label className='text-xl font-bold text-rose-500'>Company</label>
         <input
           type="text"
           placeholder="Search Company Name "
           value={filters.company}
           onChange={handleInput('company')}
-          class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          className=" my-1 px-3 py-3 placeholder-slate-300 text-slate-600 relative  bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
         />
       </div>
       <div class="mb-3 pt-0">
-        <label>Role</label>
+        <label className='text-xl font-bold text-rose-500'>Role</label>
         <input
           type="text"
           placeholder="Role :frontend,fullstack"
           value={filters.role}
           onChange={handleInput('role')}
-          class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          class=" my-1 px-3 py-3 placeholder-slate-300 text-slate-600 relative  bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
         />
       </div>
       <div class="mb-3 pt-0">
-        <label>Location</label>
+        <label className='text-xl font-bold text-rose-500'>Location</label>
         <input
           type="text"
           placeholder="remote/worldwide/usa"
           value={filters.languages}
           onChange={handleInput('location')}
-          class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          class=" my-1 px-3 py-3 placeholder-slate-300 text-slate-600 relative  bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
         />
       </div>
     </div>
